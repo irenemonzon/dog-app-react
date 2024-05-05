242223c32795202908281a27e86a77bdefd1242c
irenemonzon <irene.monzonm@gmail.com>
2024-05-05 20:26:42 +1000

    fix flex grid

--- src/components/ListAllDogs.jsx
+++ src/components/ListAllDogs.jsx
@@ -6,7 +6,7 @@ export const ListAllDogs = ({ dataDogs }) => {
       {
         dataDogs.length > 0
           ? (
-            <div className='xs:flex flex-col xs:items-center md:grid grid-cols-2 sm:gap-4 md:gap-8 lg:grid-cols-3  xl:grid-cols-4  pb-8'>
+            <div className='xs:flex flex-col xs:items-center md:grid grid-cols-2 sm:gap-4 md:gap-8 lg:grid-cols-3  xxl:grid-cols-4  pb-8'>
               {
                     dataDogs.map((dog, index) => (
                       <div className='flex justify-center items-center rounded-md border-2 border-blue-500 bg-white shadow-lg  py-8 w-60 text-center xs:mb-8 sm:mb-0' key={index}>
